ded7b0179b724b8f9946757f115f7e4bdd1a6d8e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-12 06:44:11 +0200

    create a grid view for home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../daily_lunch/daily_lunch_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -7,10 +9,39 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("IBM Malmoe"),
       ),
-      body: const Center(
-        child: Text("Under construction."),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(10.0),
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.purple[100],
+            child: TextButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const DailyLunchScreen(),
+                ),
+              ),
+              child: const Text("Ica Maxi Daily Lunch"),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.purple[200],
+            child: const Text("Kaffson"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.purple[300],
+            child: const Text("Events"),
+          ),
+        ],
       ),
     );
   }
