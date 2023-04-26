188f16c1af13299e3abee15576472c481fbd96af
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-26 03:58:55 +0200

    create enum for navigation destinations

---
+++ lib/navigation/destination.dart
@@ -0,0 +1,11 @@
+enum Destination {
+  home("Home"),
+  settings("Settings"),
+  kaffson("Kaffson"),
+  todaysLunch("Today's Lunch"),
+  events("Events");
+
+  const Destination(this.title);
+
+  final String title;
+}
