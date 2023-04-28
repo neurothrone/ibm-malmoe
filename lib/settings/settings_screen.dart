b09aa14c83d1d1abaf01090c3d5d62b8165a7d41
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-28 06:09:21 +0200

    create a switch for dark mode

--- lib/settings/settings_screen.dart
+++ lib/settings/settings_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import '../app_state.dart';
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Settings Screen"),
+    return Column(
+      children: [
+        Row(
+          children: [
+            const SizedBox(width: 10.0),
+            const Text("Dark Mode"),
+            const Spacer(),
+            Switch(
+              value: context.watch<AppState>().isDarkMode,
+              onChanged: (_) => context.read<AppState>().toggleAppTheme(),
+            ),
+            const SizedBox(width: 10.0),
+          ],
+        ),
+      ],
     );
   }
 }
