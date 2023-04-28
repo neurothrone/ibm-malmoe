d81d818bd4551b11d422e036577f8480d67c3ce8
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-28 04:30:26 +0200

    create a card button with IBM button style

---
+++ lib/home/primary_card_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class PrimaryCardButton extends StatelessWidget {
+  const PrimaryCardButton({
+    Key? key,
+    this.onPressed,
+    required this.text,
+    required this.icon,
+    this.elevation = 5.0,
+    this.padding = 10.0,
+    this.borderRadius = 20.0,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final String text;
+  final IconData icon;
+  final double elevation;
+  final double padding;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonPrimaryBackgroundColor,
+        foregroundColor: buttonPrimaryForegroundColor,
+        disabledBackgroundColor: darkButtonDisabled,
+        disabledForegroundColor: darkTextDisabled,
+        elevation: elevation,
+        padding: EdgeInsets.all(padding),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10.0),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Icon(
+                icon,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
