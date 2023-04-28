0d92c919646233dcbfdfdb46e23438085ae6ea43
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-28 04:27:39 +0200

    implement persistence for app theme

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/foundation.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'navigation/destination.dart';
 
+const isDarkModeKey = "isDarkMode";
+
 class AppState extends ChangeNotifier {
-  Destination currentDestination = Destination.home;
+  AppState() {
+    _loadPreferences();
+  }
+
+  Destination _currentDestination = Destination.home;
+  bool _isDarkMode = true;
+
+  Destination get currentDestination => _currentDestination;
+
+  bool get isDarkMode => _isDarkMode;
+
+  Future<void> _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(isDarkModeKey) ?? true;
+    notifyListeners();
+  }
 
   void navigateTo(Destination destination) {
-    currentDestination = destination;
+    _currentDestination = destination;
+    notifyListeners();
+  }
+
+  Future<void> toggleAppTheme() async {
+    _isDarkMode = !_isDarkMode;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(isDarkModeKey, _isDarkMode);
     notifyListeners();
   }
 }
