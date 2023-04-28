d2f7bb6590f1493bcf5a56e4a5c150da5bd279ff
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-29 00:48:01 +0200

    fix error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ibm_malmoe/main.dart';
+import 'package:ibm_malmoe/app_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
